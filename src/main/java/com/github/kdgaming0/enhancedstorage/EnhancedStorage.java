da0e64f9036491ff1af5910fb708bdf3d1699d57
KdGaming0 <97355992+KdGaming0@users.noreply.github.com>
2026-07-04 13:30:42 +0200

    reset

--- src/main/java/com/github/kdgaming0/enhancedstorage/EnhancedStorage.java
+++ src/main/java/com/github/kdgaming0/enhancedstorage/EnhancedStorage.java
@@ -1,9 +1,5 @@
 package com.github.kdgaming0.enhancedstorage;
 
-import com.github.kdgaming0.enhancedstorage.config.EnhancedStorageConfig;
-import com.github.kdgaming0.enhancedstorage.integration.SkyblockEnhancementsIntegration;
-import com.github.kdgaming0.enhancedstorage.storage.StorageLifecycle;
-import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +10,6 @@ public class EnhancedStorage implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        MidnightConfig.init(MOD_ID, EnhancedStorageConfig.class);
-        StorageLifecycle.init();
-        if (SkyblockEnhancementsIntegration.isPresent()) {
-            LOGGER.info("SkyBlock-Enhancements detected; deferring cursor-position saving to it while its feature is enabled.");
-        }
         LOGGER.info("Enhanced Storage initialized.");
     }
 }
